9fff31751c58a234e206696333158200516ea54e
Puneet Gupta <puneetg@outlook.com>
2017-02-23 18:41:11 +0530

    Update EasyAuthController.cs

--- AwesomeWebApp/Controllers/EasyAuthController.cs
+++ AwesomeWebApp/Controllers/EasyAuthController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Net.Http;
+using System.Net.HttpHeaders;
 
 namespace AwesomeWebApp.Controllers
 {
@@ -14,4 +19,52 @@ namespace AwesomeWebApp.Controllers
             return View();
         }
     }
+    
+    async Task<List<string>> GetGroupsFromGraphAPI(ClaimsIdentity claimsIdentity)
+        {
+            List<string> groupObjectIds = new List<string>();
+
+            string groupsClaimSourceIndex = (Json.Decode(claimsIdentity.FindFirst("_claim_names").Value)).groups;
+            var groupClaimsSource = (Json.Decode(claimsIdentity.FindFirst("_claim_sources").Value))[groupsClaimSourceIndex];
+            string requestUrl = groupClaimsSource.endpoint + "?api-version=" + ConfigurationManager.AppSettings["ida:GraphAPIVersion"];
+
+            string accesstoken = Request.Headers["X-MS-TOKEN-AAD-ID-TOKEN"].ToString();
+            // Prepare and Make the POST request
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accesstoken);
+            StringContent content = new StringContent("{\"securityEnabledOnly\": \"false\"}");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            request.Content = content;
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            // Endpoint returns JSON with an array of Group ObjectIDs
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                var groupsResult = (Json.Decode(responseContent)).value;
+
+                foreach (string groupObjectID in groupsResult)
+                    groupObjectIds.Add(groupObjectID);
+            }
+            else
+            {
+                throw new WebException();
+            }
+
+            return groupObjectIds;
+        }
+
+
+ async Task<List<string>> GetMemberGroups(ClaimsIdentity claimsIdentity)
+        {
+            //check for groups overage claim. If present query graph API for group membership
+            if (claimsIdentity.FindFirst("_claim_names") != null
+                && (Json.Decode(claimsIdentity.FindFirst("_claim_names").Value)).groups != null)
+                return await GetGroupsFromGraphAPI(claimsIdentity);
+
+            return claimsIdentity.FindAll("groups").Select(c => c.Value).ToList();
+        } 
+
+
 }
